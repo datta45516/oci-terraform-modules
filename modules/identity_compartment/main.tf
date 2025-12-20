375892cc67ff2db13b934cd58e609cd0a07a3e2e
datta45516 <sdattatray516@gmail.com>
2025-12-20 11:48:28 +0530

    Create main.tf

---
+++ modules/identity_compartment/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers { oci = { source = "oracle/oci", version = ">= 5.0.0" } }
+}
+
+resource "oci_identity_compartment" "this" {
+  compartment_id = var.parent_compartment_id
+  name           = var.name
+  description    = var.description
+  enable_delete  = var.enable_delete
+  defined_tags   = var.defined_tags
+  freeform_tags  = var.freeform_tags
+}
+
+output "id" { value = oci_identity_compartment.this.id }
+output "name" { value = oci_identity_compartment.this.name }
