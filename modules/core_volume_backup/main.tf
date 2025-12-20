d087ad83c88e2ee0c7a5b77cac70d2e8b999b89d
datta45516 <sdattatray516@gmail.com>
2025-12-20 11:32:31 +0530

    Create main.tf

---
+++ modules/core_volume_backup/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers { oci = { source = "oracle/oci", version = ">= 5.0.0" } }
+}
+
+resource "oci_core_volume_backup" "this" {
+  volume_id     = var.volume_id
+  display_name  = var.display_name
+  type          = var.type
+  defined_tags  = var.defined_tags
+  freeform_tags = var.freeform_tags
+}
+
+output "id" { value = oci_core_volume_backup.this.id }
+output "time_created" { value = oci_core_volume_backup.this.time_created }
